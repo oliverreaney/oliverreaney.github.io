31e5b3c5f8bb4ea16c1732ec99dcb08096734f3a
amvk_ <96084156+amvkk@users.noreply.github.com>
2025-06-06 23:32:24 +0100

    Add files via upload

---
+++ navscroll.js
@@ -0,0 +1,12 @@
+const navbar = document.querySelector(".navbar");
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", () => {
+  if (window.innerWidth > 768) {
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;
+    navbar.style.top = scrollTop > lastScrollTop ? "-120px" : "0";
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+  } else {
+    navbar.style.top = "0";
+  }
+});
